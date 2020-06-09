7527e3ece287a68d886f733d3b7c6b9262353445
Diego Santos <diego.osantos@g.globo>
2020-06-09 14:57:53 -0300

    Fix typo and add current work

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,14 @@ import './About.scss'
 function About(props) {
   return (
     <div className="row ds-about">
-      Eu escrevo código, projeto aplicações web e crio expeciências digitais.
+      Eu escrevo códigos, projeto aplicações web e crio experiências digitais.
+      <br />
+      <br />
+      Atualmente trabalho na&nbsp;
+      <a href="https://www.linkedin.com/company/globo-com/" target="_blank" title="Globo.com">
+        Globo.com
+      </a>
+      .
     </div>
   )
 }
